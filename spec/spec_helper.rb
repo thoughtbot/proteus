99f712a66b35875b4b4809196aaf2e4c299ed64f
Rômulo Machado <romulo.machado5@gmail.com>
2015-06-30 02:03:06 -0300

    Add helper method to silence streams

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,25 @@
 $LOAD_PATH << File.join('../lib')
 
 require 'proteus'
+
+def quietly
+  streams = STDOUT, STDERR
+  on_hold = streams.collect { |stream| stream.dup }
+  streams.each do |stream|
+    stream.reopen(null_stream)
+    stream.sync = true
+  end
+  yield
+  ensure
+    streams.each_with_index do |stream, i|
+      stream.reopen(on_hold[i])
+    end
+end
+
+def null_stream
+  if RUBY_PLATFORM =~ /mswin/
+    'NUL:'
+  else
+    '/dev/null'
+  end
+end
