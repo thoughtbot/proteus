c346b0638fddcae41088bd26a2998a50bd4fa722
Rômulo Machado <romulo.machado5@gmail.com>
2015-06-30 01:56:57 -0300

    Add tests for an existing kit
    * Before all specs remove `spec/dummy` folder used for cloning the repo
    * Add scenario for middleman kit
      * it's slow because it's not being stubbed
      * it does everything quietly (STDOUT and STDERR are being silenced)

--- spec/proteus_spec.rb
+++ spec/proteus_spec.rb
@@ -1,7 +1,8 @@
 require "spec_helper"
 
 describe Proteus do
-  before do
+  before(:all) do
+    remove_dummy_repo
     @proteus = Proteus::Kit.new
   end
 
@@ -10,7 +11,21 @@ describe Proteus do
     expect(url).to eq("git@github.com:thoughtbot/proteus-test.git")
   end
 
+  it "finds and download a kit" do
+    kit_name = 'middleman'
+    repo_name = 'spec/dummy'
+
+    quietly do
+      expect { @proteus.new(kit_name, repo_name) }.to output("Starting a new proteus-#{kit_name} project in #{repo_name}\n").to_stdout
+    end
+  end
+
   it "returns a friendly message if the repo doesn't exist" do
     expect { @proteus.new('invalid') }.to output("A thoughtbot repo doesn't exist with that name\n").to_stdout
   end
+
+  private
+    def remove_dummy_repo
+      FileUtils.rm_rf(Dir["./spec/dummy"])
+    end
 end
