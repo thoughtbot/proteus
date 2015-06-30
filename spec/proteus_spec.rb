4e9f04f8b66062bd4a85f80ef33fbed4a4c7ae7b
Rômulo Machado <romulo.machado5@gmail.com>
2015-06-30 00:35:11 -0300

    Use RSpec output matcher to test kit's output

--- spec/proteus_spec.rb
+++ spec/proteus_spec.rb
@@ -11,6 +11,6 @@ describe Proteus do
   end
 
   it "returns a friendly message if the repo doesn't exist" do
-    expect(@proteus.new('invalid')).to eq("A thoughtbot repo doesn't exist with that name")
+    expect { @proteus.new('invalid') }.to output("A thoughtbot repo doesn't exist with that name\n").to_stdout
   end
 end
